0328ee14025225bfbe22d6277a83c35ce9e63f98
newri <newri0807@naver.com>
2023-08-28 08:45:45 +0900

    chat steps EDIT

--- src/Chat.js
+++ src/Chat.js
@@ -1,51 +1,69 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from "styled-components";
 
 function Chat() {
+  const navigate = useNavigate();
+
   const steps = [
     {
       id: "0",
-      message: `갓디노님! 안녕하세요.
-           갓디노 블로그 상담 챗봇입니다.`,
+      message: "What is your name?",
       trigger: "1",
     },
     {
       id: "1",
-      message: `제 블로그의 서비스가 궁금하셨죠?            
-            그럼 소개를 시작해볼까요?
-            준비가 되셨다면 시작버튼을
-            눌러 주세요.`,
+      user: true,
       trigger: "2",
     },
     {
       id: "2",
-      options: [{ value: 1, label: "시작하기", trigger: "3" }],
+      message: "Hi {previousValue}, nice to meet you!",
+      trigger: "3",
     },
     {
       id: "3",
-      message: `블로그와 앱이 연동 되었는지 확인해주시고 연결을 해주세요.`,
-      trigger: "4",
+      options: [{ value: 1, label: "시작하기", trigger: "4" }],
     },
     {
       id: "4",
-      component: (
-        <div className="flex w-full gap-2 justify-center">
-          <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 border border-blue-700 rounded">
-            돌아가기
-          </a>
-          <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 border border-blue-700 rounded">
-            연결하기
-          </a>
-        </div>
-      ),
+      message: `블로그와 앱이 연동 되었는지 확인해주시고 연결을 해주세요.`,
+      trigger: "5",
     },
     {
       id: "5",
+      // component: (
+      //   <div className="flex w-full gap-2 justify-center">
+      //     <Link
+      //       to="/"
+      //       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 border border-blue-700 rounded"
+      //     >
+      //       돌아가기
+      //     </Link>
+      //     <button
+      //       onClick={handleConnectClick}
+      //       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 border border-blue-700 rounded"
+      //     >
+      //       연결하기
+      //     </button>
+      //   </div>
+      // ),
+      options: [
+        { value: 1, label: "연결하기", trigger: () => handleConnectClick() },
+        { value: 2, label: "이동하기", trigger: "6" },
+      ],
+    },
+    {
+      id: "6",
       component: <div>이동</div>,
     },
   ];
+
+  const handleConnectClick = () => {
+    navigate("/");
+  };
 
   const theme = {
     background: "#f5f8fb",
@@ -67,7 +85,8 @@ function Chat() {
           <ChatBot
             steps={steps}
             hideHeader={true}
-            placeholder={"채팅이 불가능한 채널입니다."}
+            placeholder={"입력해주세요"}
+            className="text-black"
           />
         </ThemeProvider>
       </header>
